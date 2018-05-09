269d57729872f1eef9387b7a1afa9943f6943fef
BrunoFR <brunofreitasrego@gmail.com>
2018-05-08 21:37:36 -0300

    aaa

--- Project1/respondernotificacao.h
+++ Project1/respondernotificacao.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "confirmacao.h"
 
 namespace Project1 {
 
@@ -34,6 +35,13 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
 
 	private:
 		/// <summary>
@@ -48,18 +56,94 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(56, 261);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(100, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Responder";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &respondernotificacao::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(386, 261);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(99, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Fechar";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &respondernotificacao::button2_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(252, 81);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(233, 22);
+			this->textBox1->TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(56, 81);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 22);
+			this->textBox2->TabIndex = 3;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(249, 61);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(68, 17);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Resposta";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(53, 61);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(152, 17);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L"Numero da Notifiçação";
+			this->label2->Click += gcnew System::EventHandler(this, &respondernotificacao::label2_Click);
 			// 
 			// respondernotificacao
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(403, 261);
+			this->ClientSize = System::Drawing::Size(570, 346);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"respondernotificacao";
 			this->Text = L"respondernotificacao";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	Close();
+}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	confirmacao^ caixa = gcnew confirmacao();
+	caixa->ShowDialog();
+}
+};
 }
